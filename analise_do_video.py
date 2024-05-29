7b6952033887a9ef1d979c68674b05367d5e49ef
Gustavo <gustavohenriquealves418@gmail.com>
2024-05-28 21:03:03 -0300

    2 versão

--- analise_do_video.py
+++ analise_do_video.py
@@ -1,18 +1,24 @@
 import numpy as np 
 import cv2 as cv 
+import sys
+class analise:
+    def exibindo_video_img(self,video,img):
+        cap = cv.VideoCapture("video1.avi")
+        while cap.isOpened():
+            ret,frame = cap.read()
+            # Caso o câmera não abrir 
+            if not ret:
+                break 
+            gray = cv.cvtColor(frame,cv.COLOR_BAYER_BG2BGR)
+            cv.imshow('frame',gray)
+            if cv.waitKey(1) == ord("q"):
+                break 
+            cap.release()
+            cv.destroyAllWindows()
+        
 
-def exibindo_video():
-    cap = cv.VideoCapture("video1.avi")
-    while cap.isOpened():
-        ret,frame = cap.read()
-        # Caso o câmera não abrir 
-        if not ret:
-            break 
-        gray = cv.cvtColor(frame,cv.COLOR_BAYER_BG2BGR)
-        cv.imshow('frame',gray)
-        if cv.waitKey(1) == ord("q"):
-            break 
-        cap.release()
-        cv.destroyAllWindows()
+    def analise_video():
+        pass 
 
-exibindo_video()
+    def analise_img():
+        pass 
