128040a4b73bf0e17d8e631a4d53bbe82463ec86
Gustavo <gustavohenriquealves418@gmail.com>
2024-05-28 21:06:45 -0300

    2 versão

--- analise_do_video.py
+++ analise_do_video.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import sys
 class analise:
     def exibindo_video_img(self,video,img):
+        self.img = img
         cap = cv.VideoCapture("video1.avi")
         while cap.isOpened():
             ret,frame = cap.read()
@@ -15,7 +16,13 @@ class analise:
                 break 
             cap.release()
             cv.destroyAllWindows()
-        
+        img = cv.imread(cv.samples.findFile("foto.png"))
+        if img is None:
+            sys.exit("Imagem inexistente")
+        cv.imshow("mostrar janela",img)
+        quit = cv.waitKey(0)
+        if quit == "q":
+            cv.imwrite("foto.png",img)
 
     def analise_video():
         pass 
