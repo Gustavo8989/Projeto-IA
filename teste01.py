7d67676a2577f3c987164a55d03789a9dc313f0c
Gustavo <gustavohenriquealves418@gmail.com>
2024-06-13 13:27:51 -0300

    Ajustando o gitignore

--- teste01.py
+++ teste01.py
@@ -4,8 +4,4 @@ imagem = cv.imread('Imagens/img01.jpg')
 (b,g,r) = imagem[0,0]
 
 
-def analise_img():
-    # Modificando a imagem
-    for y in range(0, imagem.shape[0]):
-        for x in range(0, imagem.shape[1]):
-            imagem[y,x] =  (255,0,0)
+
