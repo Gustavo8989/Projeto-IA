686dfaeb0b18f1eee14a6a17bb7757dbab7f4222
Gustavo <gustavohenriquealves418@gmail.com>
2024-06-19 18:29:47 -0300

    Corrigindo bug

--- reconhecimento_sensoriais/analise_img.py
+++ reconhecimento_sensoriais/analise_img.py
@@ -8,18 +8,26 @@ def escrever(imagem,texto,cor=(255,0,0)):
     fonte = cv.FONT_HERSHEY_SIMPLEX 
     cv.putText(imagem,texto,(10,20),fonte,0.5, cor, 0, cv.LINE_AA) 
 
-imagem = cv.cvtoColor(imagem_colorida, cv.COLOR_BRG2GRAY)
+imagem = cv.cvtColor(imagem_colorida, cv.COLOR_BGR2GRAY)
 suave = cv.blur(imagem,(7,7)) 
 # Binarização da imagem, resultando em pixel branco e preto 
 
 T = mahotas.thresholding.otsu(suave)
-bin = sauve.copy() 
+bin = suave.copy() 
 bin[bin > T] = 255 
 bin[bin < 255] = 0
 bin = cv.bitwise_not(bin) 
 
 bordas = cv.Canny(suave,70,150) 
 
-(lix,objetos,lix) = cv.findContours(bordas.copy(),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
-# A váriavel lix é lixo, os dados que não são utilizados 
-
+(lix,objetos) = cv.findContours(bordas.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) # A váriavel lix é lixo, os dados que não são utilizados 
+escrever(imagem,"Imagem de tons de cinza", 0)
+escrever(suave, "Suavização com bluer", 0) 
+escrever(bin, "Binarização com método Otsu",255) 
+escrever(bordas,"Detector de bordas Canny",255) 
+temp = np.stack([
+    np.hstack([imagem,suave]),
+    np.hstack([bin,bordas]) 
+]) 
+cv.imshow("Quantidade de objetos"+str(len(objetos)),temp) 
+cv.waitKey(0) 
